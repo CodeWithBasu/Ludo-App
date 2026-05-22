ce9ee879a47228304f94ddd63681f0a8716bb085
Basudev Moharana <basudevmuna111@gmail.com>
2026-05-22 08:36:00 +0530

    Render pawns on board

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ludo_app/models/game_state.dart';
 import 'package:ludo_app/models/player.dart';
+import 'package:ludo_app/models/pawn.dart';
 import 'package:ludo_app/logic/game_logic.dart';
+import 'package:ludo_app/logic/path_coordinates.dart';
 import 'package:ludo_app/widgets/board_widget.dart';
+import 'package:ludo_app/widgets/pawn_widget.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -26,10 +29,16 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
+  void _onPawnTapped(Pawn pawn) {
+    setState(() {
+      GameLogic.movePawn(_gameState, pawn);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFE8EAF6), // Light indigo background
+      backgroundColor: const Color(0xFFE8EAF6),
       appBar: AppBar(
         title: const Text('Pass & Play'),
         backgroundColor: Colors.indigo,
@@ -39,11 +48,30 @@ class _GameScreenState extends State<GameScreen> {
         child: Column(
           children: [
             _buildPlayerInfo(),
-            const Expanded(
+            Expanded(
               child: Center(
                 child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: BoardWidget(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      // Calculate the square size for the board
+                      double boardSize = constraints.maxWidth < constraints.maxHeight
+                          ? constraints.maxWidth
+                          : constraints.maxHeight;
+                      double cellSize = boardSize / 15;
+
+                      return SizedBox(
+                        width: boardSize,
+                        height: boardSize,
+                        child: Stack(
+                          children: [
+                            const BoardWidget(),
+                            ..._buildPawns(boardSize, cellSize),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
@@ -52,6 +80,57 @@ class _GameScreenState extends State<GameScreen> {
         ),
       ),
     );
+  }
+
+  List<Widget> _buildPawns(double boardSize, double cellSize) {
+    List<Widget> pawnWidgets = [];
+    
+    for (var color in PlayerColor.values) {
+      List<Pawn> pawns = _gameState.pawns[color]!;
+      for (int i = 0; i < pawns.length; i++) {
+        Pawn pawn = pawns[i];
+        
+        List<int> gridCoords;
+        if (pawn.state == PawnState.atBase) {
+           gridCoords = PathCoordinates.basePositions[color.name]![i];
+        } else if (pawn.state == PawnState.atHome) {
+           // Put it near the center home
+           gridCoords = [7, 7]; 
+        } else {
+           if (pawn.position < 52) {
+             int absPos = GameLogic.getAbsoluteBoardPosition(pawn.color, pawn.position);
+             gridCoords = PathCoordinates.mainPath[absPos];
+           } else {
+             int homeIndex = pawn.position - 52;
+             gridCoords = PathCoordinates.homePaths[pawn.color.name]![homeIndex];
+           }
+        }
+
+        double x = gridCoords[0] * cellSize + (cellSize * 0.15); // Offset to center pawn
+        double y = gridCoords[1] * cellSize + (cellSize * 0.15);
+
+        // Adjust slightly if at home to cluster them
+        if (pawn.state == PawnState.atHome) {
+          x += (i % 2) * (cellSize * 0.3) - (cellSize * 0.15);
+          y += (i ~/ 2) * (cellSize * 0.3) - (cellSize * 0.15);
+        }
+
+        pawnWidgets.add(
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            left: x,
+            top: y,
+            child: PawnWidget(
+              pawn: pawn,
+              size: cellSize * 0.7,
+              onTap: () => _onPawnTapped(pawn),
+            ),
+          ),
+        );
+      }
+    }
+    return pawnWidgets;
   }
 
   Widget _buildPlayerInfo() {
@@ -128,7 +207,7 @@ class _GameScreenState extends State<GameScreen> {
       case PlayerColor.green:
         return Colors.green;
       case PlayerColor.yellow:
-        return Colors.amber; // amber looks better than pure yellow
+        return Colors.amber;
       case PlayerColor.blue:
         return Colors.blue;
     }
